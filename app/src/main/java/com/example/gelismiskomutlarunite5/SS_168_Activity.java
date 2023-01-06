4bfd19dbf69f2b950b04ec4886265a9ebb5c3763
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-01-06 13:07:46 +0300

    no message

--- app/src/main/java/com/example/gelismiskomutlarunite5/SS_168_Activity.java
+++ app/src/main/java/com/example/gelismiskomutlarunite5/SS_168_Activity.java
@@ -11,82 +11,44 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SS_168_Activity extends AppCompatActivity {
 
-    Button btnTopla, btnCikar, btnCarp, btnBol;
-    EditText txt1, txt2;
+
+    EditText textNumber1, textNumber2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ss_168_activity);
 
-        btnTopla = findViewById(R.id.btnSsCalistir165_Topla);
-        btnTopla.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int sayi1 = Integer.parseInt(txt1.getText().toString());
-                int sayi2 = Integer.parseInt(txt2.getText().toString());
-
-                int toplam = uyg3calistirtoplama(sayi1, sayi2);
-                Toast.makeText(getApplicationContext(), "Sonuç: " + toplam, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnCikar = findViewById(R.id.btnSsCalistir165_Cikar);
-        btnCikar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int sayi1 = Integer.parseInt(txt1.getText().toString());
-                int sayi2 = Integer.parseInt(txt2.getText().toString());
-
-                int cikarma = uyg3calistircikarma(sayi1, sayi2);
-                Toast.makeText(getApplicationContext(), "Sonuç: " + cikarma, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnBol = findViewById(R.id.btnSsCalistir165_Bolme);
-        btnBol.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int sayi1 = Integer.parseInt(txt1.getText().toString());
-                int sayi2 = Integer.parseInt(txt2.getText().toString());
-
-                int bol = uyg3calistirbolme(sayi1, sayi2);
-                Toast.makeText(getApplicationContext(), "Sonuç: " + bol, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnCarp = findViewById(R.id.btnSsCalistir165_Carpma);
-        btnCarp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int sayi1 = Integer.parseInt(txt1.getText().toString());
-                int sayi2 = Integer.parseInt(txt2.getText().toString());
-
-                int carpma = uyg3calistircarpma(sayi1, sayi2);
-                Toast.makeText(getApplicationContext(), "Sonuç: " + carpma, Toast.LENGTH_SHORT).show();
-            }
-        });
-
+        textNumber1 = findViewById(R.id.editTxtSs168_1);
+        textNumber2 = findViewById(R.id.editTxtSs168_2);
     }
 
-    private int uyg3calistirtoplama(int sayi1, int sayi2)
-    {
-            return sayi1 + sayi2;
-
+    public void btnToplama(View view) {
+        int result = hesapla(Integer.parseInt(textNumber1.getText().toString()), Integer.parseInt(textNumber2.getText().toString()), "+");
+        Toast.makeText(this, "Toplam: " + result, Toast.LENGTH_SHORT).show();
     }
 
-    private int uyg3calistircikarma(int sayi1, int sayi2)
-    {
-        return sayi1 - sayi2;
+    public void btnCikarma(View view) {
+        int result = hesapla(Integer.parseInt(textNumber1.getText().toString()), Integer.parseInt(textNumber2.getText().toString()), "-");
+        Toast.makeText(this, "Çıkarma: " + result, Toast.LENGTH_SHORT).show();
     }
 
-    private int uyg3calistirbolme(int sayi1, int sayi2)
-    {
-        return sayi1 / sayi2;
+    public void btnCarpma(View view) {
+        int result = hesapla(Integer.parseInt(textNumber1.getText().toString()), Integer.parseInt(textNumber2.getText().toString()), "*");
+        Toast.makeText(this, "Çarpma: " + result, Toast.LENGTH_SHORT).show();
     }
 
-    private int uyg3calistircarpma(int sayi1, int sayi2)
-    {
-        return sayi1 * sayi2;
+    public void btnBolme(View view) {
+        int result = hesapla(Integer.parseInt(textNumber1.getText().toString()), Integer.parseInt(textNumber2.getText().toString()), "/");
+        Toast.makeText(this, "Bölme: " + result, Toast.LENGTH_SHORT).show();
+    }
+
+    public int hesapla(int sayi1, int sayi2, String islem) {
+        int number = 0;
+        if(islem == "+") number = sayi1 + sayi2;
+        else if(islem == "-") number = sayi1 - sayi2;
+        else if(islem == "*") number = sayi1 * sayi2;
+        else if(islem == "/") number = sayi1 / sayi2;
+        return number;
     }
 }
